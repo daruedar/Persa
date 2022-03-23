70e7fa18e5e3fb9f06946e1bedcaf921face5fdb
Diego Armando Rueda Robles <daruedar@hotmail.com>
2022-03-22 20:18:02 -0500

    Update Gui.java

--- Gui.java
+++ Gui.java
@@ -1,10 +1,11 @@
 package vista;
-	import javax.swing.JButton;
-	import javax.swing.JPanel;
-	public class Gui extends JPanel {
-			JButton b1;
-			public Gui(){
+import javax.swing.JButton;
+import javax.swing.JPanel;
+public class Gui extends JPanel 
+	{
+		JButton b1;
+		public Gui()	{
 					b1= new JButton("Cielo");
 					add(b1);
 				}
-		}
+	}
